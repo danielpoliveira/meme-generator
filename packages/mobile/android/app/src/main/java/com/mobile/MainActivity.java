df6c10ed35b41fe71e7aa1fb977ab3594bb2fd1d
danielpoliveira <dani.edm@outlook.com>
2020-08-01 03:15:15 -0300

    Add textinput, move, and rotate

---
+++ packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mobile;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;                           // + react-native-gesture-handler
+import com.facebook.react.ReactRootView;                                   // + react-native-gesture-handler
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // + react-native-gesture-handler
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mobile";
+  }
+
+  @Override                                                             // + react-native-gesture-handler
+  protected ReactActivityDelegate createReactActivityDelegate() {       // + react-native-gesture-handler
+    return new ReactActivityDelegate(this, getMainComponentName()) {    // + react-native-gesture-handler
+      @Override                                                         // + react-native-gesture-handler
+      protected ReactRootView createRootView() {                        // + react-native-gesture-handler
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);   // + react-native-gesture-handler
+      }                                                                 // + react-native-gesture-handler
+    };                                                                  // + react-native-gesture-handler
+  }                                                                     // + react-native-gesture-handler
+}

dd93c8f2db200f4829cd6e671bc49dd4195856d7
danielpoliveira <dani.edm@outlook.com>
2020-08-02 00:34:07 -0300

    Add routes and your configurations

---
+++ packages/mobile/src/routes.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import Home from './pages/Home';
+import Insert from './pages/Insert';
+
+const Tab = createBottomTabNavigator();
+
+export default () => {
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color }) => <MaterialIcons name="home" size={30} color={color} />
+          }}
+          name="Home"
+          component={Home}
+        />
+        <Tab.Screen
+          name="Insert"
+          component={Insert}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
